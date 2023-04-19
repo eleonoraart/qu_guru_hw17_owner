bc5c36deaa2b06e1bc137c440200af21d99684ae
Eleonora Artyukhova <zhooggas@gmail.com>
2023-04-20 02:18:24 +0600

    add properties

--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -24,7 +24,6 @@ public interface WebConfig extends Config {
         boolean isRemote();
 
         @Key("remoteUrl")
-        @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
         String remoteUrl();
 
 }
